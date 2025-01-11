ede8dc059413fe886c27184e919592b42667762b
Gustavo <gzarruk@gmail.com>
2025-01-11 22:24:07 +0100

    Add Money Flow Index (MFI) indicator and refactor indicator generation

--- galgoz/indicators/trend.py
+++ galgoz/indicators/trend.py
@@ -23,26 +23,3 @@ class SG(Indicator):
             self.data.values, window_length=self.window, polyorder=self.order
         )
         self.output = pd.Series(res, index=self.data.index, name="SG")
-
-
-class MFI(Indicator):
-    window: int = 14
-
-    def __init__(self, data: pd.DataFrame, window: int = 14):
-        super().__init__(name="Money Flow Index", data=data)
-        self.window = window
-        if data is not None:
-            self.run()
-
-    def __str__(self):
-        return f"Money Flow Index (window={self.window})"
-
-    def run(self):
-        res = mfi(
-            self.data["mid_h"],
-            self.data["mid_l"],
-            self.data["mid_c"],
-            self.data["volume"],
-            timeperiod=self.window,
-        )
-        self.output = pd.Series(res, index=self.data.index, name="MFI")
---
+++ galgoz/indicators/volume.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from galgoz.indicators.base import Indicator
+from talib import MFI as mfi
+
+
+class MFI(Indicator):
+    row: int = 2
+    window: int = 14
+
+    def __init__(self, data: pd.DataFrame, window: int = 14):
+        super().__init__(name="Money Flow Index", data=data)
+        self.window = window
+        if data is not None:
+            self.run()
+
+    def __str__(self):
+        return f"Money Flow Index (window={self.window})"
+
+    def run(self):
+        res = mfi(
+            self.data["mid_h"],
+            self.data["mid_l"],
+            self.data["mid_c"],
+            self.data["volume"],
+            timeperiod=self.window,
+        )
+        self.output = pd.Series(res, index=self.data.index, name="MFI")

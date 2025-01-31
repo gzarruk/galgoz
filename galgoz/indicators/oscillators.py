9ad821c69c7de153ab3f50322766cf435fe9541a
Gustavo <gzarruk@gmail.com>
2025-01-31 08:03:53 +0100

    Add QQE indicator implementation and refactor qqe function for improved performance

--- galgoz/indicators/oscillators.py
+++ galgoz/indicators/oscillators.py
@@ -59,17 +59,49 @@ class RSI(Indicator):
         self.run()
 
 
+class QQE(Indicator):
+    row: int = 2
+    length: int = 8
+    smooth: int = 1
+    factor: float = 1.618
+
+    def __init__(
+        self,
+        data: pd.DataFrame,
+        length: int = length,
+        smooth: int = smooth,
+        factor: float = factor,
+        **kwargs,
+    ):
+        super().__init__(name="QQE", data=data)
+        self.length = length
+        self.smooth = smooth
+        self.factor = factor
+        if data is not None:
+            self.run()
+        self._update_attributes(kwargs)
+
+    def __str__(self):
+        return f"Quantitative Qualitative Estimation (length={self.length})"
+
+    def run(self):
+        result = qqe(
+            self.data, length=self.length, smooth=self.smooth, factor=self.factor
+        )
+        self.output = result[["qqe_fast", "qqe_slow"]]
+
+    def update(self, new_data: pd.DataFrame | None):
+        self.data = new_data
+        self.run()
+
+
 def qqe(
     data: pd.DataFrame, length: int = 8, smooth: int = 1, factor: float = 1.618
 ) -> pd.DataFrame:
     df = data.copy()
-    qqe_fast = (
-        pd.Series(rsi(df["mid_c"].values, timeperiod=length), index=df.index)
-        .ewm(span=smooth)
-        .mean()
-        .values
-    )
-    df["qqe_fast"] = qqe_fast
+    _rsi = rsi(df["mid_c"].to_numpy(dtype=np.float64), timeperiod=length)
+    _qqe_fast = pd.Series(data=_rsi, index=df.index).ewm(span=smooth).mean().to_numpy()
+    df["qqe_fast"] = _qqe_fast
     rsi_delta = df["qqe_fast"].diff().abs().values
     wmma = np.zeros(len(df))
     atr_rsi = np.zeros(len(df))
@@ -80,31 +112,29 @@ def qqe(
         atr_rsi[i] = wmma[i] / length + (1 - 1 / length) * (
             atr_rsi[i - 1] if not np.isnan(atr_rsi[i - 1]) else 0
         )
-    qqe_up = qqe_fast + atr_rsi * factor
-    qqe_down = qqe_fast - atr_rsi * factor
-    qqe_down = df.qqe_fast.values - atr_rsi * factor
+    qqe_up = _qqe_fast + atr_rsi * factor
+    qqe_down = _qqe_fast - atr_rsi * factor
 
     qqe_slow = np.zeros(len(df))
     for i in range(1, len(df)):
         if qqe_up[i] < (qqe_slow[i - 1] if not np.isnan(qqe_slow[i - 1]) else 0):
             qqe_slow[i] = qqe_up[i]
-        elif qqe_fast[i] > (
+        elif _qqe_fast[i] > (
             qqe_slow[i - 1] if not np.isnan(qqe_slow[i - 1]) else 0
-        ) and qqe_fast[i - 1] < (
+        ) and _qqe_fast[i - 1] < (
             qqe_slow[i - 1] if not np.isnan(qqe_slow[i - 1]) else 0
         ):
             qqe_slow[i] = qqe_down[i]
         elif qqe_down[i] > (qqe_slow[i - 1] if not np.isnan(qqe_slow[i - 1]) else 0):
             qqe_slow[i] = qqe_down[i]
-        elif qqe_fast[i] < (
+        elif _qqe_fast[i] < (
             qqe_slow[i - 1] if not np.isnan(qqe_slow[i - 1]) else 0
-        ) and qqe_fast[i - 1] > (
+        ) and _qqe_fast[i - 1] > (
             qqe_slow[i - 1] if not np.isnan(qqe_slow[i - 1]) else 0
         ):
             qqe_slow[i] = qqe_up[i]
         else:
             qqe_slow[i] = qqe_slow[i - 1]
 
-    df["qqe_fast"] = qqe_fast
     df["qqe_slow"] = qqe_slow
     return df

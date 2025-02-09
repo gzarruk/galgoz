6586e0a6339b70efdb3275281d075e82b253a16d
Gustavo <gzarruk@gmail.com>
2025-02-09 08:52:43 +0100

    Refactor SuperTrend implementation to use helper function for improved readability and maintainability

--- galgoz/indicators/trend.py
+++ galgoz/indicators/trend.py
@@ -85,63 +85,24 @@ class SuperTrend(Indicator):
         )
 
     def run(self):
-        high = self.data["mid_h"]
-        low = self.data["mid_l"]
-        close = self.data["mid_c"]
+        st = _supertrend(
+            self.data,
+            self.atr_period,
+            self.multiplier,
+        )
 
-        # Calculate ATR
-        tr1 = high - low
-        tr2 = abs(high - close.shift(1))
-        tr3 = abs(low - close.shift(1))
-        tr = pd.concat([tr1, tr2, tr3], axis=1).max(axis=1)
-        atr = tr.rolling(window=self.atr_period).mean()
-
-        # Calculate Supertrend
-        hl2 = (high + low) / 2
-        upper_band = hl2 + (self.multiplier * atr)
-        lower_band = hl2 - (self.multiplier * atr)
-
-        final_upper_band = upper_band.copy()
-        final_lower_band = lower_band.copy()
-
-        for i in range(1, len(self.data)):
-            if close.iloc[i] > final_upper_band.iloc[i - 1]:
-                final_upper_band.iloc[i] = upper_band.iloc[i]
-            else:
-                final_upper_band.iloc[i] = min(
-                    upper_band.iloc[i], final_upper_band.iloc[i - 1]
-                )
-
-            if close.iloc[i] < final_lower_band.iloc[i - 1]:
-                final_lower_band.iloc[i] = lower_band.iloc[i]
-            else:
-                final_lower_band.iloc[i] = max(
-                    lower_band.iloc[i], final_lower_band.iloc[i - 1]
-                )
-
-        supertrend = pd.Series(index=self.data.index)
-        in_uptrend = True
-        for i in range(1, len(self.data)):
-            if close.iloc[i] > final_upper_band.iloc[i - 1]:
-                in_uptrend = True
-            elif close.iloc[i] < final_lower_band.iloc[i - 1]:
-                in_uptrend = False
-
-            if in_uptrend:
-                supertrend.iloc[i] = final_lower_band.iloc[i]
-            else:
-                supertrend.iloc[i] = final_upper_band.iloc[i]
-
-        self.output = supertrend
+        self.output = st[0]
 
     def update(self, new_data: pd.DataFrame | None):
         self.data = new_data
         self.run()
 
+
 def _supertrend(data, atr_period=14, multiplier=6.5):
     # Instance of supertrend indicator used to generate the SuperTrend class
     res = supertrend(data.mid_h, data.mid_l, data.mid_c, atr_period, multiplier)
     return res
+
 
 def supertrend(
     high: pd.Series,
